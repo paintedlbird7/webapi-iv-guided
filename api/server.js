2e332dc53f05039c643f67b43faf73c22db4d952
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-04 10:05:35 -0700

    motd

--- api/server.js
+++ api/server.js
@@ -11,7 +11,9 @@ server.use(express.json());
 server.get('/', async (req, res) => {
   try {
     const shoutouts = await db('shoutouts');
-    res.status(200).json(shoutouts);
+    const messageOfTheDay = process.env.MOTD || 'Hello World'
+    res.status(200).json({motd: messageOfTheDay, shoutouts});
+
   } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
@@ -23,7 +25,9 @@ server.post('/', async (req, res) => {
     const [id] = await db('shoutouts').insert(req.body);
     const shoutouts = await db('shoutouts');
 
-    res.status(201).json(shoutouts);
+    const messageOfTheDay = process.env.MOTD || 'Hello World'
+    res.status(200).json({motd: messageOfTheDay, shoutouts})
+    
   } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot add the shoutout' });
